91c231915a791dc90ece778cbb4875f8aa360440
Sai Ashirwad <saiashirwadr@gmail.com>
2025-02-13 21:54:55 +0530

    wip

---
+++ src/data/video-testimonials.ts
@@ -0,0 +1,14 @@
+export const videoTestimonials = [
+	"https://www.youtube.com/watch?v=Fvzu7kTlah4&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=23&t=2s&pp=gAQBiAQB",
+	"https://www.youtube.com/watch?v=EN-j9HA7V9Y&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=34&t=76s&pp=gAQBiAQB",
+	"https://www.youtube.com/watch?v=r4ZKpgnBVuw&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=69&t=22s&pp=gAQBiAQB",
+	"https://www.youtube.com/watch?v=qDoG-aD3fjQ&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=90&pp=gAQBiAQB",
+	"https://www.youtube.com/watch?v=nFZTs7A1jfo&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=101&t=1193s&pp=gAQBiAQB",
+	"https://www.youtube.com/watch?v=ztcQ6BWFspQ&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=104&t=573s&pp=gAQBiAQB",
+	"https://www.youtube.com/watch?v=BbTQXCg1uJ8&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=108&t=319s&pp=gAQBiAQB",
+	"https://www.youtube.com/watch?v=xSEtcUxdN7A&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=110&t=14s&pp=gAQBiAQB",
+	"https://www.youtube.com/watch?v=-mB6VdlaQ7k&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=115&t=389s&pp=gAQBiAQB",
+	"https://www.youtube.com/watch?v=m-Kr1MGuCkk&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=118&pp=gAQBiAQB",
+	"https://www.youtube.com/watch?v=uca-GPR-UBg&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=119&t=34s&pp=gAQBiAQB",
+	"https://www.youtube.com/watch?v=JNkw-Y5lST0&list=PLRmko9r3esDGBv0eqUZ2mSAFc50-dnRk3&index=126&t=364s&pp=gAQBiAQB",
+];
